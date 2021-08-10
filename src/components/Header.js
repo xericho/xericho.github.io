f6add03f0d287eef5a3dc66cb1b222a0b82b0e34
Eric Ho <xeric.hox@gmail.com>
2021-08-10 15:06:04 -0700

    removed transition-delay; changed icon to terminal

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,13 @@ import React from 'react'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-diamond"></span>
+      <span className="icon fa-terminal"></span>
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>Eric Ho</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+            Welcome! I'm a machine learning engineer!
         </p>
       </div>
     </div>
