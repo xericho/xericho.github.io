9a1976b8e5eed12f9dd6c3c762ba3e6208e95221
Eric Ho <xeric.hox@gmail.com>
2022-05-30 01:19:21 -0700

    blank chakra template with navbar and hero

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+} from '@chakra-ui/react';
+import { Hero } from './components/Hero'
+import { Navbar } from './components/Navbar'
+import theme from './theme';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl" p={10}>
+        <Navbar />
+        <Grid minH="100vh" p={3}>
+          <Hero id='test' />
+        </Grid>
+        <Grid minH="100vh" p={3}>
+          <Hero id='blah'/>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
